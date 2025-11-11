414ac3abad1c6a6057c5f4faf058466df185025d
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2025-11-11 12:07:46 +0000

    Gamaplay Tag initialized

---
+++ Source/GASCrash/Private/GameplayTags/GCTags.cpp
@@ -0,0 +1,10 @@
+﻿#include "GameplayTags/GCTags.h"
+
+namespace GCTags
+{
+	namespace GCAbilities
+	{
+		// Tag and value
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "GCTags.GC.Abilities.Primary", "Tag for the Primary Ability")
+	}
+}
---
+++ Source/GASCrash/Public/GameplayTags/GCTags.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace GCTags
+{
+	namespace GCAbilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary)
+	}
+}
